1c66f3905f6bbc00b8167281792761f99363782b
jadonamite <jadonamite@gmail.com>
2026-03-30 15:54:23 +0100

    refactor: improve core logic structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,14 @@
-import Hero from '@/components_/landing/Hero'
-import Features from '@/components_/landing/Features'
-import { FreeCTA, Footer } from '@/components_/landing/CTAFooter'
+import Hero from_ '@/components_/landing/Hero'
+import Features from_ '@/components_/landing/Features'
+import { FreeCTA, Footer } from_ '@/components_/landing/CTAFooter'
 
 export default function LandingPage() {
   return (
-    <main_>
+    <main>
       <Hero />
       <Features />
       <FreeCTA />
       <Footer />
-    </main_>
+    </main>
   )
 }

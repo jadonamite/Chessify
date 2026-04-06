2bea075cd018a29cd6d361f933a0036242440490
jadonamite <jadonamite@gmail.com>
2026-04-06 15:49:32 +0100

    feat: implement core logic logic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Features from '@/components_/landing/Features'
 import Hero from '@/components_/landing/Hero'
 import { FreeCTA, Footer } from '@/components_/landing/CTAFooter'
 
+// ← the muse was here
 export default function LandingPage() {
   return (
     <main>

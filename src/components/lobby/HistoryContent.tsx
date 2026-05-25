b975fc4eeb72bc59c8a55001fa01485e3c8f5a8b
jadonamite <jadonamite@gmail.com>
2026-05-25 14:43:32 +0100

    docs: add JSDoc annotation to function in HistoryContent.tsx

--- src/components/lobby/HistoryContent.tsx
+++ src/components/lobby/HistoryContent.tsx
@@ -10,6 +10,10 @@ import LoadingState from '@/components/ui/LoadingState'
 import { useHistory } from '@/hooks/useHistory'
 import { Queen, PieceView } from '@/components/ui/ChessModels'
 
+/**
+ * Scene
+ * @returns {*}
+ */
 function Scene() {
   return (
     <>

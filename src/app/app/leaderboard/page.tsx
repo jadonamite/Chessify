00463342b65882449d3df2fa79e2e1c14f8f3736
jadonamite <jadonamite@gmail.com>
2026-06-11 18:58:56 +0100

    docs: add JSDoc annotation to function in page.tsx

--- src/app/app/leaderboard/page.tsx
+++ src/app/app/leaderboard/page.tsx
@@ -1,5 +1,9 @@
 import LeaderboardContent from '@/components/lobby/LeaderboardContent'
 
+/**
+ * LeaderboardPage
+ * @returns {*}
+ */
 export default function LeaderboardPage() {
   return <LeaderboardContent />
 }

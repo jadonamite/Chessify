a5e853f4cefc7fce485d176a0f39b60cbf65a2e2
jadonamite <jadonamite@gmail.com>
2026-06-11 20:52:56 +0100

    docs: add TODO note for future optimization in page.tsx

--- src/app/app/history/page.tsx
+++ src/app/app/history/page.tsx
@@ -2,4 +2,5 @@ import { HistoryContent } from '@/components/lobby/HistoryContent'
 
 export default function HistoryPage() {
   return <HistoryContent />
+// NOTE: revisit this logic after API migration
 }

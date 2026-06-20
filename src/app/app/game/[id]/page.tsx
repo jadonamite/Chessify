ac0d58e65be96da50f837995bba81416569dfa3f
jadonamite <jadonamite@gmail.com>
2026-06-20 22:29:57 +0100

    refactor(app): refactor page, layout

--- src/app/app/game/[id]/page.tsx
+++ src/app/app/game/[id]/page.tsx
@@ -1,10 +1,14 @@
 'use client'
 import dynamic from 'next/dynamic'
 
-const loadGameClient = () =>
-  dynamic(() => import('@/components/game/GameClient'), { ssr: false })
+const loadGameClient = () => dynamic(() => import('@/components/game/GameClient'), { ssr: false })
+
+export const useGameClient = () => {
+  const GameClient = loadGameClient()
+  return GameClient
+}
 
 export default function GamePage() {
-  const GameClient = loadGameClient()
+  const GameClient = useGameClient()
   return <GameClient />
 }

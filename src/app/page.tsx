39269438c3b7310987cbe345080ba8ab3db79f48
jadonamite <jadonamite@gmail.com>
2026-07-03 00:47:54 +0100

    feat(app): add LandingPage component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
 import ChessifyLanding from '@/components/landing/v2/ChessifyLanding'
 
+/**
+ * LandingPage
+ * @returns {*}
+ */
 export default function LandingPage() {
   return <ChessifyLanding />
 }
